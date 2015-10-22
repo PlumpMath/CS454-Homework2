d53cbb18e0301a2c4015a9c24c92fe824a470b0c
CharlesMrl <charles.mariller@yahoo.fr>
2015-10-22 16:30:38 -0700

    Adding database

---
+++ HW2DB.sql
@@ -0,0 +1,26 @@
+use hw2db;
+
+
+drop table players;
+
+create table players(
+	name varchar(30),
+	password varchar(20),
+	character varchar(30),
+	posX DECIMAL(4,4),
+	posY DECIMAL(4,4),
+	posZ DECIMAL(4,4),
+	posH DECIMAL(4,4), 
+	posP DECIMAL(4,4),
+	posR DECIMAL(4,4),
+
+
+	PRIMARY KEY (name)
+
+
+);
+
+insert into players VALUES ('Vicken','1234','ralph',0,0,0,0,0,0);
+insert into players VALUES ('Genus','1234','panda',3,3,0,0,0,0);
+insert into players VALUES ('Maxime','1234','car',5,5,0,0,0,0);
+insert into players VALUES ('Charles','1234','car',7,7,0,0,0,0);
