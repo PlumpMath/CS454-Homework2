015bf650607d362082eccb9e914b8285a64532fe
MaximeMourier <maxmourier@hotmail.com>
2015-10-24 23:30:24 -0700

    Continued Chat GUI

--- assets/chat.py
+++ assets/chat.py
@@ -1,5 +1,7 @@
 import random, sys, os, math, time
-from direct.gui.DirectGui import DirectFrame, DirectEntry
+from direct.gui.DirectGui import *
+#from direct.gui.DirectGui import DirectFrame, DirectEntry
+#import direct.directbase.DirectStart
 
 class Chat:
     def __init__(self, world):
@@ -8,6 +10,7 @@ class Chat:
         self.hideChat()
         self.chat = 0
         self.loadControls()
+        self.msgPosition = 1.95
         taskMgr.add(self.showChat,"Show Chat")
 
     def hideChat(self):
@@ -15,7 +18,7 @@ class Chat:
 
     def showChat(self, task):
         if(self.chat != 0):
-            print("Showing Chat")
+            #print("Showing Chat")
             self.chatFrame.show()
             self.chat = 0
         return task.cont
@@ -37,10 +40,14 @@ class Chat:
             #TO DO : implent function send message
             if(textEntered == "/q" or textEntered == "/Q"):
                 self.hideChat()
+            else:
+                #send message
+                self.printMessage("Maxime",textEntered)
 
         #clear the text
         def clearText():
-            entry.enterText('')
+            #entry.enterText('')
+            test = "test"
 
         entry = DirectEntry(
                     parent = chatFrame,
@@ -51,9 +58,24 @@ class Chat:
                     numLines = 1,
                     focus=0,
                     focusInCommand=clearText,
-                    width= 20,
-                    )
+                    width= 20)
         entry.setPos(-0.327, 0, -0.46)
-        print(entry.getBounds())
-
+        self.scrollFrame = DirectScrolledFrame(
+                        parent = chatFrame,
+                        manageScrollBars = True,
+                        autoHideScrollBars = True,
+                        scrollBarWidth = 0.05,
+                        canvasSize = (-.5,.45,-2,2),
+                        frameSize = (-.5,.51,-.5,.5))
+        self.scrollFrame.setPos(0.17, 0, 0.1)
         return chatFrame
+
+    def printMessage(self, user, msg):
+        label = DirectLabel(
+            parent = self.scrollFrame.getCanvas(),
+            text = user+" : "+msg,
+            scale = .05
+            )
+        width = label.getWidth()
+        label.setPos(-.48+(width/40),0,self.msgPosition)
+        self.msgPosition -= 0.05
