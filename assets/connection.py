1971329410b5aa6b62b2410dd09469e112e42b39
Vicken52 <krikorian.vicken@gmail.com>
2015-10-25 21:44:27 -0700

    Created template Requests and Responses for Client

--- assets/connection.py
+++ assets/connection.py
@@ -35,33 +35,33 @@ class Connection(ShowBase):
 		except:
 			pass
 
-	def message(self, task):
-		self.option = 0
-		self.option = str(raw_input("1-Send integer\n2-Send string\n3-Send short\n4-Send float\n"))
-		if self.option == "1":
-			msg = int(100 * random.random()) - 50
-			request = self.intRequest(msg)
-			self.cWriter.send(request,self.connection)
-			print "sent ", msg
-			taskMgr.remove('message')
-		elif self.option == "2":
-			msg = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(7))
-			request = self.stringRequest(msg)
-			self.cWriter.send(request,self.connection)
-			print "sent ", msg
-			taskMgr.remove('message')
-		elif self.option == "3":
-			msg = int(100 * random.random())
-			request = self.shortRequest(msg)
-			self.cWriter.send(request,self.connection)
-			print "sent ", msg
-			taskMgr.remove('message')
-		elif self.option == "4":
-			msg = 100 * random.random()
-			request = self.floatRequest(msg)
-			self.cWriter.send(request,self.connection)
-			print "sent ", msg
-			taskMgr.remove('message')
+	# def message(self, task):
+	# 	self.option = 0
+	# 	self.option = str(raw_input("1-Send integer\n2-Send string\n3-Send short\n4-Send float\n"))
+	# 	if self.option == "1":
+	# 		msg = int(100 * random.random()) - 50
+	# 		request = self.intRequest(msg)
+	# 		self.cWriter.send(request,self.connection)
+	# 		print "sent ", msg
+	# 		taskMgr.remove('message')
+	# 	elif self.option == "2":
+	# 		msg = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(7))
+	# 		request = self.stringRequest(msg)
+	# 		self.cWriter.send(request,self.connection)
+	# 		print "sent ", msg
+	# 		taskMgr.remove('message')
+	# 	elif self.option == "3":
+	# 		msg = int(100 * random.random())
+	# 		request = self.shortRequest(msg)
+	# 		self.cWriter.send(request,self.connection)
+	# 		print "sent ", msg
+	# 		taskMgr.remove('message')
+	# 	elif self.option == "4":
+	# 		msg = 100 * random.random()
+	# 		request = self.floatRequest(msg)
+	# 		self.cWriter.send(request,self.connection)
+	# 		print "sent ", msg
+	# 		taskMgr.remove('message')
 
 	def intRequest(self, msg):
 		pkg = PyDatagram()
@@ -87,11 +87,42 @@ class Connection(ShowBase):
 		pkg.addFloat32(msg)
 		return pkg
 
-	def registerRequest(self, username, password):
+	def loginRequest(self, username, password):
 		pkg = PyDatagram()
-		pkg.addUint16(102)
+		pkg.addUint16(101)
 		pkg.addString(username)
 		pkg.addString(password)
+		return pkg
+
+	def registerRequest(self, username, password):
+		pkg = PyDatagram()
+		pkg.addUint16(103)
+		pkg.addString(username)
+		pkg.addString(password)
+		return pkg
+
+	def characterCreationRequest(self, username, classType):
+		pkg = PyDatagram()
+		pkg.addUint16(104)
+		pkg.addString(username)
+		pkg.addUint16(0)
+		pkg.addUint16(classType)
+		return pkg
+
+	def chatRequest(self, message):
+		pkg = PyDatagram()
+		pkg.addUint16(105)
+		pkg.addString(message)
+		return pkg
+
+	def moveRequest(self, x, y, z, h, isMoving):
+		pkg = PyDatagram()
+		pkg.addUint16(106)
+		pkg.addFloat32(x)
+		pkg.addFloat32(y)
+		pkg.addFloat32(z)
+		pkg.addFloat32(h)
+		pkg.addUint16(isMoving)
 		return pkg
 
 	def check(self):
@@ -111,6 +142,16 @@ class Connection(ShowBase):
 					self.getShort(data)
 				elif responseCode == 4:
 					self.getFloat(data)
+				elif responseCode == 201:
+					self.getLogin(data)
+				elif responseCode == 203:
+					self.getRegistion(data)
+				elif responseCode == 204:
+					self.getCharacterCreation(data)
+				elif responseCode == 205:
+					self.getChat(data)
+				elif responseCode == 206:
+					self.getMove(data)
 				else:
 					print "nothing found"
 
@@ -134,6 +175,59 @@ class Connection(ShowBase):
 		print "recieved ", msg
 		taskMgr.add(self.message, 'message')
 
+	def getLogin(self, data):
+		flag = data.getUint16()
+
+		if(flag == 0):
+			print "Invalid Login"
+		else:
+			listSize = data.getUint16()
+
+			for i in xrange(listSize):
+				characterName = data.getString()
+				characterId = data.getUint16()
+				characterType = data.getUint16()
+				characterFaction = data.getUint16()
+
+				# Need to add these to an active characters class
+
+
+	def getRegistion(self, data):
+		flag = data.getUint16()
+
+		if(flag == 0):
+			print "Invalid Login"
+		else:
+			print "Account Created"
+
+	def getCharacterCreation(self, data):
+		flag = data.getUint16()
+
+		if(flag == 0):
+			print "Invalid Character"
+		else:
+			listSize = data.getUint16()
+
+			for i in xrange(listSize):
+				characterName = data.getString()
+				characterId = data.getUint16()
+				characterType = data.getUint16()
+				characterFaction = data.getUint16()
+
+				# Need to add these to an active characters class
+
+	def getChat(self, data):
+		username = data.getString()
+		factionId = data.getUint16()
+		message = data.getString()
+
+	def getMove(self, data):
+		username = data.getString()
+		x = data.getFloat32()
+		y = data.getFloat32()
+		z = data.getFloat32()
+		h = data.getFloat32()
+		isMoving = data.getUint16()
 
 	def updateRoutine(self,task):
 		self.check()
